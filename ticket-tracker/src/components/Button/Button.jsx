b3e97be1d6a3fab8ca79a0feca2b22e8b391e6f0
Roscoe Bull <roscoe-bull@live.co.uk>
2021-12-03 01:13:04 +0000

    basic styling applied

--- ticket-tracker/src/components/Button/Button.jsx
+++ ticket-tracker/src/components/Button/Button.jsx
@@ -2,15 +2,8 @@ import "./Button.scss";
 
 
 const Button =(props)=>{
-   let buttonStyles = "button"
-   
-   if (props.isMinus===true){
-      buttonStyles +=" button--minus"
-   }else{
-      buttonStyles +=" button--plus"
-   }
- 
-   return <button className={buttonStyles}>{props.buttonText}</button>
+    
+   return <button className="button">{props.buttonText}</button>
 };
 
 export default Button
